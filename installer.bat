770dd0c4bc912d36ba3fe9354621b40b12253f1a
ARIHANT KOTHARI <151532001+arihant4521k@users.noreply.github.com>
2025-07-10 08:28:27 +0000

    added installer file

---
+++ installer.bat
@@ -0,0 +1,5 @@
+bat
+@echo off
+echo Installing Python packages from requirements.txt...
+pip install -r requirements.txt
+pause
---
+++ main.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import os
+import threading
+import yt_dlp
+from tkinter import filedialog
